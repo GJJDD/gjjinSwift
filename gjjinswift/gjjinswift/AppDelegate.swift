9f4cfe04d67e4d81f985d59d17447e0a0df0b822
高佳杰 <434779154@qq.com>
2016-06-29 17:32:36 +0800

    初始化工程，设置tabBar

---
+++ gjjinswift/gjjinswift/AppDelegate.swift
@@ -0,0 +1,122 @@
+//
+//  AppDelegate.swift
+//  gjjinswift
+//
+//  Created by apple on 16/6/28.
+//  Copyright © 2016年 QiaTu HangZhou. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
+        
+        self.window = UIWindow.init(frame: UIScreen.main().bounds)
+        self.window?.rootViewController = setuptabBarController()
+        self.window?.makeKeyAndVisible()
+        
+        return true
+    }
+    
+    
+    func setuptabBarController() -> UIViewController {
+        
+        let inTabBarController = UITabBarController()
+        inTabBarController.tabBar.backgroundColor = UIColor.white()
+        inTabBarController.tabBar.tintColor = UIColor.init(red: 220/255.0, green: 92/255.0, blue: 108/255.0, alpha: 1)
+        // 关注
+        let aboutController = UIViewController()
+        aboutController.view.backgroundColor = UIColor.red()
+        aboutController.title = "关注"
+        
+        aboutController.tabBarItem.image = UIImage.init(named: "grayfriend")
+        aboutController.tabBarItem.selectedImage = UIImage.init(named: "redfriend")
+        // 发现
+        let findController = UIViewController()
+        findController.title = "发现"
+        findController.tabBarItem.image = UIImage.init(named: "graydiscover")
+        findController.tabBarItem.selectedImage = UIImage.init(named: "reddiscover")
+        
+        // 拍照
+        let takePhotoController = UIViewController()
+        takePhotoController.tabBarItem.image = UIImage.init(named: "paizhao")
+  
+
+        
+        // in记
+        let inController = UIViewController()
+        inController.title = "in记"
+        inController.tabBarItem.image = UIImage.init(named: "grayinji")
+        inController.tabBarItem.selectedImage = UIImage.init(named: "redinji")
+        // 中心
+        let centerController = UIViewController()
+        centerController.title = "中心"
+        centerController.tabBarItem.image = UIImage.init(named: "graycenter")
+        centerController.tabBarItem.selectedImage = UIImage.init(named: "redcenter")
+        inTabBarController.viewControllers = [aboutController, findController,takePhotoController, inController, centerController]
+        
+        // 取消上边线
+        UITabBar.appearance().backgroundImage = UIImage.init()
+        UITabBar.appearance().shadowImage = UIImage.init()
+        
+        inTabBarController.tabBar .insertSubview(drawTabbarBgImageView(), at: 0)
+        inTabBarController.tabBar.isOpaque = true
+        
+        let tabBarItem = inTabBarController.tabBar.items?[2]
+        tabBarItem?.imageInsets = UIEdgeInsets.init(top: -3, left: 0, bottom: 3, right: 0)
+        
+
+        return inTabBarController
+    }
+    
+    func drawTabbarBgImageView() -> UIImageView {
+        let radius = 30.0 // 圆半径
+        let tabBarHeight = 49.0
+        let standOutHeight = 12.0 // 突出的高度
+        let allFloat = (pow(Decimal(radius), 2)-pow(Decimal(radius-standOutHeight), 2))
+        let ww = sqrtf(Float(allFloat))
+        let imageView = UIImageView.init(frame: CGRect.init(x: 0.0, y: -standOutHeight, width: Double(UIScreen.main().bounds.size.width), height: tabBarHeight+standOutHeight))
+        let size = imageView.frame.size
+        let layer = CAShapeLayer.init()
+        let path = UIBezierPath.init()
+        path.move(to: CGPoint.init(x: size.width/2-CGFloat(ww), y: CGFloat(standOutHeight)))
+        
+        let angleH = 0.5*((radius-standOutHeight)/radius)
+        
+        let startAngle = (1+angleH)*M_PI
+        let endAngle = (2-angleH)*M_PI
+    
+        let center = CGPoint.init(x: CGFloat((size.width)/2), y: CGFloat(radius))
+        path.addArc(withCenter:center , radius: CGFloat(radius), startAngle: CGFloat(startAngle), endAngle: CGFloat(endAngle), clockwise: true)
+            
+        path.addLine(to: CGPoint.init(x: CGFloat(size.width/2+(CGFloat)(ww)), y: CGFloat(standOutHeight)))
+        path.addLine(to: CGPoint.init(x: size.width, y:
+            CGFloat(standOutHeight)))
+        
+        path.addLine(to: CGPoint.init(x: size.width, y: size.height))
+        
+        path.addLine(to: CGPoint.init(x: 0, y: size.height))
+        
+        path.addLine(to: CGPoint.init(x: 0, y: standOutHeight))
+        
+        path.addLine(to: CGPoint.init(x: size.width/2-CGFloat(ww), y: CGFloat(standOutHeight)))
+        layer.path = path.cgPath
+        layer.fillColor = UIColor.white().cgColor
+        
+        layer.strokeColor = UIColor.init(white: 0.765, alpha: 1.0).cgColor
+        layer.lineWidth = 0.5
+        
+        imageView.layer .addSublayer(layer)
+     
+        return imageView;
+    }
+
+}
+
+
+
---
+++ gjjinswift/gjjinswift/GJJLaunchViewController.swift
@@ -0,0 +1,31 @@
+//
+//  GJJLaunchViewController.swift
+//  gjjinswift
+//
+//  Created by apple on 2016/6/29.
+//  Copyright © 2016年 QiaTu HangZhou. All rights reserved.
+//
+
+import UIKit
+
+class GJJLaunchViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+      
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
